5a9a0dd2c2d250ed85820ad0e64474cd63b00f3c
thsm <theo@seelye.net>
2022-01-19 23:15:17 -0800

    random based on username and selection working

--- app.ts
+++ app.ts
@@ -8,7 +8,7 @@ const app = express();
 // const sqlite3 = require('sqlite3');
 // const sqlite = require('sqlite');
 import sqlite3 from "sqlite3";
-import sqlite from "sqlite";
+import {open} from "sqlite";
 
 const INVALID_PARAM_ERROR = 400;
 const SEVER_ERROR = 500;
@@ -16,7 +16,20 @@ const SERVER_ERROR_MSG = 'Something went wrong on the server.';
 
 app.use(express.static('public'));
 
-
+app.get("/string", async function (req, res) {
+  const user = req.query.user;
+  if (!user) {
+    res.type("text");
+    res.status(INVALID_PARAM_ERROR).send("Error: missing username query parameter.");
+  } else {
+    let db = await getDBConnection();
+    const query = 
+        "select * from strings where id not in (select strid from responses where user like ?) order by random() limit 1;";
+    let respstring = await db.all(query, user);
+    db.close();
+    res.json(respstring);
+  }
+});
 
 /**
  * Establishes a database connection to a database and returns the database object.
@@ -25,12 +38,12 @@ app.use(express.static('public'));
  * @returns {Object} - The database object for the connection.
  */
  async function getDBConnection() {
-    const db = await sqlite.open({
-      filename: 'data.sqlite3',
-      driver: sqlite3.Database
-    });
-    return db;
-  }
+  const db = await open({
+    filename: 'data.db',
+    driver: sqlite3.Database
+  });
+  return db;
+}
   
-  const PORT = process.env.PORT || 8000;
-  app.listen(PORT);
+const PORT = process.env.PORT || 8000;
+app.listen(PORT);
